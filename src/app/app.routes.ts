40afddf32709937b23d49601b1315f670b401cac
Gennadij Lobotskij <utyf185@mail.ru>
2025-05-05 12:37:25 +0300

    Feat: verify

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,6 @@ export const routes: Routes = [
   {path: '', redirectTo: 'sign-in', pathMatch: 'full'},
   {path: 'sign-in', title: "chess notes", loadComponent: () => import('./features/sign-in/sign-in.component').then(m => m.SignInComponent)},
   {path: 'sign-up',loadComponent: () => import('./features/sign-up/sign-up.component').then(m => m.SignUpComponent) , title: "chess notes registration"},
+  {path: 'verify/:token', title: "verify", loadComponent: () => import('./features/verify/verify.component').then(m => m.VerifyComponent)},
   {path: '**', redirectTo: 'sign-in'}
 ];
--- src/app/features/verify/verify.component.ts
+++ src/app/features/verify/verify.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {AuthService} from "../../core/services/auth/auth.service";
+import {NgIf} from "@angular/common";
+import {LoaderComponent} from "../../shared/components/loader/loader.component";
+import {delay} from "rxjs";
 
 @Component({
   selector: 'app-verify',
   standalone: true,
-  imports: [],
+  imports: [NgIf, LoaderComponent],
   templateUrl: './verify.component.html',
   styleUrl: './verify.component.scss'
 })
-export class VerifyComponent {
+export class VerifyComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private auth: AuthService
+  ) {
+  }
+  isLoading:boolean = true
+  isVerify:boolean = false
+  ngOnInit() {
+    const token = this.route.snapshot.paramMap.get('token')
+    if(token){
+      this.auth.confirmEmail(token).pipe(delay(500)).subscribe({
+        next: res=> {
+          console.log(res.message)
+          this.isLoading = false
+          this.isVerify = true
+        },
+        error: err=> {
+          console.log(err.error)
+          this.isLoading = false
+          this.isVerify = false
+        }
+      })
+    }
 
+  }
 }
