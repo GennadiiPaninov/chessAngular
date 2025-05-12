d3b964b7da1dd420308c1a58834e3227f35ef5d8
Gennadij Lobotskij <utyf185@mail.ru>
2025-05-12 16:07:27 +0300

    Refactor: authGuard

--- src/app/core/helpers/authGuard/authGuard.ts
+++ src/app/core/helpers/authGuard/authGuard.ts
@@ -3,10 +3,15 @@ import {inject} from "@angular/core";
 import {AuthService} from "../../services/auth/auth.service";
 import {catchError, map, of} from "rxjs";
 
-export const authGuard: CanActivateFn =(route, state)=>{
+export const authGuard: CanActivateFn = (route, state)=>{
   const authService = inject(AuthService)
   const router = inject(Router)
+  const accessToken = localStorage.getItem('access_token')
 
+  if (!accessToken) {
+    router.navigate(['/sign-in'], { queryParams: { returnUrl: state.url } })
+    return of(false);
+  }
   return authService.checkAuth().pipe(
     map(() => true),
     catchError(() => {
