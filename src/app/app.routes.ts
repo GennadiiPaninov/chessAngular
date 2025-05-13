0a6add76b8ce93c4e27f890c4a1787836dbfc6e8
Gennadij Lobotskij <utyf185@mail.ru>
2025-05-13 10:22:37 +0300

    Feat: auth, authorized layouts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,41 +7,47 @@ import {provideState} from "@ngrx/store";
 import {verifyReducer} from "./store/verify/verify.reducer";
 import {LoginEffects} from "./store/login/login.effects";
 import {redirectAuthGuard} from "./core/helpers/redirectAuthGuard/redirectAuthGuard";
+import {AuthLayoutComponent} from "./shared/layouts/auth-layout/auth-layout.component";
+import {AuthorizedLayoutComponent} from "./shared/layouts/authorized-layout/authorized-layout.component";
 
 export const routes: Routes = [
   {
     path: '',
-    title: "chess notes",
-    loadComponent: () => import('./features/main-page/main-page.component').then(m => m.MainPageComponent),
-    canActivate: [authGuard]
+    component: AuthorizedLayoutComponent,
+    canActivate: [authGuard],
+    children: [
+      {
+        path: '',
+        title: 'chess notes',
+        loadComponent: () => import('./features/main-page/main-page.component').then(m => m.MainPageComponent)
+      },
+    ]
   },
   {
-    path: 'sign-in',
-    title: "chess notes",
-    loadComponent: () => import('./features/sign-in/sign-in.component').then(m => m.SignInComponent),
-    providers: [
-      provideEffects(LoginEffects)
-    ],
-    canActivate: [redirectAuthGuard]
-  },
-  {
-    path: 'sign-up',
-    loadComponent: () => import('./features/sign-up/sign-up.component').then(m => m.SignUpComponent),
-    title: "chess notes registration",
-    providers: [
-      provideEffects(RegisterEffects)
-    ],
-    canActivate: [redirectAuthGuard]
-  },
-
-  {
-    path: 'verify/:token',
-    title: "verify",
-    loadComponent: () => import('./features/verify/verify.component').then(m => m.VerifyComponent),
-    providers: [
-      provideState('verify', verifyReducer),
-      provideEffects(VerifyEffects)
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'sign-in',
+        title: 'chess notes',
+        canActivate: [redirectAuthGuard],
+        loadComponent: () => import('./features/sign-in/sign-in.component').then(m => m.SignInComponent),
+        providers: [provideEffects(LoginEffects)]
+      },
+      {
+        path: 'sign-up',
+        title: 'chess notes registration',
+        canActivate: [redirectAuthGuard],
+        loadComponent: () => import('./features/sign-up/sign-up.component').then(m => m.SignUpComponent),
+        providers: [provideEffects(RegisterEffects)]
+      },
+      {
+        path: 'verify/:token',
+        title: 'verify',
+        loadComponent: () => import('./features/verify/verify.component').then(m => m.VerifyComponent),
+        providers: [provideState('verify', verifyReducer), provideEffects(VerifyEffects)]
+      }
     ]
   },
   {path: '**', redirectTo: ''}
-];
+]
---
+++ src/app/shared/layouts/authorized-layout/authorized-layout.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from "../../components/header/header.component";
+import {RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-authorized-layout',
+  standalone: true,
+    imports: [
+        HeaderComponent,
+      RouterOutlet
+    ],
+  templateUrl: './authorized-layout.component.html',
+  styleUrl: './authorized-layout.component.scss'
+})
+export class AuthorizedLayoutComponent {
+
+}
